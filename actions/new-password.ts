36b9afd95c7f2eacc25937b3fa15a9f1a163990a
dipaloke <dipalokebiswas96@gmail.com>
2024-04-18 19:57:56 +0600

    New-Password Page: added new-password to auth route, created new password page with newPasswordForm component, created validation schema for password reset & integrate into new password form, created server action for newPassword and integrate into newPasswordForm. NEXT: 2FA(Two fector authentication for credentials).

---
+++ actions/new-password.ts
@@ -0,0 +1,57 @@
+"use server";
+import { db } from "@/lib/db";
+import * as z from "zod";
+import { NewPasswordSchema } from "@/schemas";
+import { getPasswordResetTokenByToken } from "@/data/password-reset-token";
+import { getUserByEmail } from "@/data/user";
+import bcrypt from "bcryptjs";
+
+export const newPassword = async (
+  values: z.infer<typeof NewPasswordSchema>,
+  token: string | null
+) => {
+  if (!token) {
+    return { error: "Missing Token" };
+  }
+
+  const validatedFields = NewPasswordSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid Fields!" };
+  }
+
+  const { password } = validatedFields.data;
+
+  const existingToken = await getPasswordResetTokenByToken(token);
+
+  if (!existingToken) {
+    return { error: "Invalid Token!" };
+  }
+
+  const tokenHasExpired = new Date(existingToken.expires) < new Date();
+
+  if (tokenHasExpired) {
+    return { error: "Token has expired!" };
+  }
+
+  const existingUser = await getUserByEmail(existingToken.email);
+  if (!existingUser) {
+    return { error: "Email doesn't exists!" };
+  }
+
+  //hashing new password and update
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await db.user.update({
+    where: { id: existingUser.id },
+    data: {
+      password: hashedPassword,
+    },
+  });
+
+  await db.passwordResetToken.delete({
+    where: { id: existingToken.id },
+  });
+
+  return { success: "Password updated!" };
+};
---
+++ components/auth/new-password-form.tsx
@@ -0,0 +1,100 @@
+"use client";
+import { CardWrapper } from "./card-wrapper";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { NewPasswordSchema } from "@/schemas";
+import { Button } from "../ui/button";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+import { useState, useTransition } from "react";
+import { useSearchParams } from "next/navigation";
+import { CircleIcon } from "@radix-ui/react-icons";
+
+import { newPassword } from "@/actions/new-password";
+
+export const NewPasswordForm = () => {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof NewPasswordSchema>>({
+    resolver: zodResolver(NewPasswordSchema),
+    defaultValues: {
+      password: "",
+    },
+  });
+
+  //TODO: form onSubmit functionality
+  const onSubmit = (values: z.infer<typeof NewPasswordSchema>) => {
+    //clearing up the messages
+    setError("");
+    setSuccess("");
+
+    startTransition(() => {
+      newPassword(values, token).then((data) => {
+        setError(data?.error);
+        setSuccess(data?.success);
+      });
+    });
+  };
+
+  return (
+    <CardWrapper
+      headerLabel="Enter a new password."
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
+    >
+      <Form {...form}>
+        <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="******"
+                      type="password"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button type="submit" className="w-full">
+            {isPending ? (
+              <span className="flex items-center space-x-2">
+                <CircleIcon className="h-5 w-5 animate-spin" />
+                <span>Resetting...</span>
+              </span>
+            ) : (
+              "Reset password"
+            )}
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
